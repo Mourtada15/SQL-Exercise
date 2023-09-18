1d1bfaabc4a3684760c6e8bd5ec311f28732b237
Hassan Mourtada <hassanmourtada15>
2023-09-18 16:54:03 +0300

    Count & Filter

--- answers.sh
+++ answers.sh
@@ -19,6 +19,16 @@ UPDATE students SET Points = 400 WHERE name = "Basma"
 # Decrease the points of Alex
 UPDATE students SET Points = 200-100 WHERE name = "Alex"
 
+# Create table
+CREATE TABLE IF NOT EXISTS graduates (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    Name TEXT NOT NULL,
+    Age INTEGER NOT NULL,
+    Gender TEXT NOT NULL,
+    Points INTEGER NOT NULL,
+    Graduates DATE NOT NULL
+);
+
 # Copy Layal's data from students to graduates 
 INSERT INTO graduates (name, Age, Gender, Points, graduates)
 SELECT name, Age, Gender, Points, 01/01/\
@@ -33,4 +43,15 @@ WHERE name = 'Layal'
 # Remove Layal's record from students
 DELETE FROM students WHERE ID = 4
 
-# 
+# Produce a table that contains, for each employee, his/her name, company name, and company date.
+CREATE TABLE my_company AS 
+SELECT employees.name, employees.Company, companies.Date
+FROM employees
+INNER JOIN companies 
+ON employees.Company = companies.Name ;
+
+# Find the name of employees that work in companies made before 2000.
+SELECT name FROM my_company WHERE date < 2000
+
+# Find the name of company that has a graphic designer.
+SELECT Company FROM employees WHERE Role = "Graphic Designer"
